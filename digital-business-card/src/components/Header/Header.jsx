978b7947b13e4c5520e8cd66f2f31abfedc9f648
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2022-08-10 21:47:04 +0530

    Completed the Header Componenent

--- digital-business-card/src/components/Header/Header.jsx
+++ digital-business-card/src/components/Header/Header.jsx
@@ -1,25 +1,33 @@
 import React from "react";
-import './Header.css';
+import "./Header.css";
 
 function Header() {
-    return (
-        <header className="header-part">
-            <img src="public/PASSPORTSIZEPHOTO.jpeg" alt=""/>
-            <div className="credentials">
-                <h1>Soumyadeep Chatterjee</h1>
-                <h4>Frontend Developer</h4>
-                <a href="https://github.com/Soumya98-dev">soumyadeep.website</a>
-            </div>
-            <div className="btns">
-                <a href="mailto: soumyadeepchatterjee803@gmail.com">
-                    <img src="public/Button.png"/>
-                </a>
-                <a className="linkedin-btn" href="https://www.linkedin.com/in/soumyadeep-chatterjee-60a086210/">
-                    <img className="linkedin-btn-img" src="public/LinkedIn-Button.png"/>
-                </a>
-            </div>
-        </header>
-    );
+  return (
+    <header className="header-part">
+      <img src="public/PASSPORTSIZEPHOTO.jpeg" alt="" />
+      <div className="credentials">
+        <h1>Soumyadeep Chatterjee</h1>
+        <h4>Frontend Developer</h4>
+        <a href="https://github.com/Soumya98-dev">soumyadeep.website</a>
+      </div>
+      <div className="btns">
+        <a
+          className="email-btn"
+          href="mailto: soumyadeepchatterjee803@gmail.com"
+        >
+          <i className="fa-solid fa-envelope"></i>
+          Email
+        </a>
+        <a
+          className="linkedin-btn"
+          href="https://www.linkedin.com/in/soumyadeep-chatterjee-60a086210/"
+        >
+          <i className="fa-brands fa-linkedin"></i>
+          LinkedIn
+        </a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
