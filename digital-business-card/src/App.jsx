5584731e02653520cfc290922c1e1a32f75d3bf3
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2022-08-11 01:16:43 +0530

    Completed the middle content

--- digital-business-card/src/App.jsx
+++ digital-business-card/src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import MiddleContent from "./components/MiddleContent/MiddleContent.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
-    return (
-        <>
-            <Header/>
-            <MiddleContent/>
-            <Footer/>
-        </>
-    )
+  return (
+    <main>
+      <div className="container">
+        <Header />
+        <MiddleContent />
+        <Footer />
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- digital-business-card/src/components/MiddleContent/MiddleContent.jsx
+++ digital-business-card/src/components/MiddleContent/MiddleContent.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import "./MiddleContent.css";
 
-function MiddleContent(){
-    return(
-        <div>
-
-        </div>
-    )
+function MiddleContent() {
+  return (
+    <div className="middle-content">
+      <section className="about">
+        <h1>About</h1>
+        <p>
+          I am a frontend developer with a particular interest in making things
+          simple and automating daily tasks. I try to keep up with security and
+          best practices, and am always looking for new things to learn.
+        </p>
+      </section>
+      <h1>Interests</h1>
+      <p>
+        Food expert. Music scholar. Reader. Internet fanatic. Pizza buff. Travel
+        geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </div>
+  );
 }
 
 export default MiddleContent;
