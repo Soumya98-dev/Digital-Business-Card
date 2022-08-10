9bcf6484004b4e14d481d9e326e206d28c914744
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2022-08-11 02:19:52 +0530

    Added the footer component

--- digital-business-card/src/components/Footer/Footer.jsx
+++ digital-business-card/src/components/Footer/Footer.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import './Footer.css';
 
-function Footer(){
-    return(
-        <div>
-
-        </div>
-    )
+function Footer() {
+  return (
+    <div className="footer">
+      <a href="https://twitter.com/soumyachatty">
+        <i className="fa-brands fa-square-twitter"></i>
+      </a>
+      <a href="https://www.facebook.com/soumyadeep.chatterjee.7906">
+        <i className="fa-brands fa-square-facebook"></i>
+      </a>
+      <a href="https://www.instagram.com/soumyadeep.chatterjee98/">
+        <i className="fa-brands fa-square-instagram"></i>
+      </a>
+      <a href="https://github.com/Soumya98-dev?tab=overview&from=2022-08-01&to=2022-08-10">
+        <i className="fa-brands fa-square-github"></i>
+      </a>
+    </div>
+  );
 }
 
 export default Footer;
